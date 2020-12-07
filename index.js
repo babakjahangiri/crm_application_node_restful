82d0fe718ca1a1712e23b921307b16b7e88cb56a
Babak <babakjahangiri@gmail.com>
2020-12-07 02:03:16 +0000

    monitor request on console

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import express from "express";
+import routes from "./src/routes/crmRoutes";
 
 const app = express();
 
 const PORT = 5000;
 
+routes(app);
+
 app.get('/',(req,res) => {
   res.send(`Node & Express running on port ${PORT}`)
 })
 
-app.listen(PORT, ()=>
+app.listen(PORT, () =>
   console.log(`Server is running on port ${PORT}`)
 );
